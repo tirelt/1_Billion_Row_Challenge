ecd7a233bc1b7fc96c90facc6717212c5fdd4a17
Mac theo <theotirel@gmail.com>
2026-01-18 12:52:24 +0800

    bang pattern

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,18 +1,22 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Main (main) where
-import qualified Data.Map as M
+import qualified Data.Map.Strict as M
 import Control.Monad
 import Data.List
+import qualified Data.ByteString.Lazy.Char8 as B
 
 main :: IO ()
 main = do
-    contents <- readFile "../data/measurements.txt"  
-    let res = foldl' update M.empty (lines contents)
-    print $ M.lookup "Bytom" res
+    contents <- B.readFile "../data/measurements.txt"  
+    let res = foldl' update M.empty (B.lines contents)
+    print $ M.lookup (B.pack "Bytom") res
 
-update map line = 
+update ::M.Map B.ByteString (Float, Float, Float, Int) -> B.ByteString -> M.Map B.ByteString (Float, Float, Float, Int)
+update !map !line = 
     let 
-        (city,_:tempString) = break (==';') line
-        temp = read tempString :: Float
+        (city,rest) = B.break (==';') line
+        temp = read (B.unpack (B.drop 1 rest)) :: Float
     in M.insertWith aggregate city (temp,temp,temp,1) map
 
 aggregate (oldMin,oldMax,oldSum,oldCount) (newMin,newMax,newSum,newCount) = (min oldMin newMin,max oldMax newMax, oldSum + newSum,oldCount+newCount)
