1c16259509f9e909ea13a31a81fe1c77348a3eec
Mac theo <theotirel@gmail.com>
2026-01-18 13:43:07 +0800

    custom parser

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE BangPatterns #-}
 
 module Main (main) where
-import qualified Data.Map.Strict as M
-import Control.Monad
-import Data.List
+import qualified Data.HashMap.Strict as M
+import Data.List (foldl')
 import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Char (digitToInt)
 
 main :: IO ()
 main = do
@@ -12,13 +12,26 @@ main = do
     let res = foldl' update M.empty (B.lines contents)
     print $ M.lookup (B.pack "Bytom") res
 
-update ::M.Map B.ByteString (Float, Float, Float, Int) -> B.ByteString -> M.Map B.ByteString (Float, Float, Float, Int)
-update !map !line = 
+update ::M.HashMap B.ByteString (Int, Int, Int, Int) -> B.ByteString -> M.HashMap B.ByteString (Int, Int, Int, Int)
+update !accMap !line = 
     let 
         (city,rest) = B.break (==';') line
-        temp = read (B.unpack (B.drop 1 rest)) :: Float
-    in M.insertWith aggregate city (temp,temp,temp,1) map
+        temp = parseTemp rest
+    in M.insertWith aggregate city (temp,temp,temp,1) accMap
 
+parseTemp :: B.ByteString -> Int
+parseTemp = parse 0 
+    where 
+        parse acc bs 
+            | B.null bs = acc
+            | h == '-' = -parse acc t 
+            | h == '.' || h ==';' = parse acc t
+            | otherwise = parse (10*acc + digitToInt h) t
+            where
+                h = B.head bs
+                t = B.tail bs
+
+aggregate :: (Ord a, Ord b, Num c, Num d) => (a, b, c, d) -> (a, b, c, d) -> (a, b, c, d)
 aggregate (oldMin,oldMax,oldSum,oldCount) (newMin,newMax,newSum,newCount) = (min oldMin newMin,max oldMax newMax, oldSum + newSum,oldCount+newCount)
 
 
